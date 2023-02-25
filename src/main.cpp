7c92f0e1dee78c89eb65820a062ec4cf69db409c
Gothic-Gargoyle <rijndershylbren@gmail.com>
2023-02-25 19:23:16 +0100

    Beginning of PWM control

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
+int potmeter = A0;
+int ledPin = 11; // configureer de LED pin.
+
 void setup() {
-    // initialize digital pin LED_BUILTIN as an output.
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600); // zet seriele poort aan op 9600 baud.
+    pinMode(ledPin, OUTPUT); // zet de LED pin als uitgang
 }
 
-// the loop function runs over and over again forever
-void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(2000);                      // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(1000);                      // wait for a second
+void loop()
+{
+    int potentiometerValue = analogRead(potmeter);
+    int brightness = potentiometerValue / 4;
+    analogWrite(ledPin, brightness);
 }
